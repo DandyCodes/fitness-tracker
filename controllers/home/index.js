0bbd1a571b18430b5d835e77a59f8bac16505dd4
John Barker <jdabarker@gmail.com>
2021-07-05 23:17:26 +0800

    added redirect routes to public html files

--- controllers/home/index.js
+++ controllers/home/index.js
@@ -1,4 +1,12 @@
 const router = require("express").Router();
 const db = require("../../models");
 
+router.get("/stats", (req, res) => {
+  res.redirect("/stats.html");
+});
+
+router.get("/exercise", (req, res) => {
+  res.redirect("/exercise.html");
+});
+
 module.exports = router;
