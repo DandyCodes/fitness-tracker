e12a0b85a714984aeb27bd94e17de501257691c2
John Barker <jdabarker@gmail.com>
2021-07-06 13:18:16 +0800

    changed routing from redirect to sendfile

--- controllers/home/index.js
+++ controllers/home/index.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
-const db = require("../../models");
+const path = require("path");
 
-router.get("/stats", (req, res) => {
-  res.redirect("/stats.html");
+router.get("/stats", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/stats.html"));
 });
 
-router.get("/exercise", (req, res) => {
-  res.redirect("/exercise.html");
+router.get("/exercise", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/exercise.html"));
 });
 
 module.exports = router;
