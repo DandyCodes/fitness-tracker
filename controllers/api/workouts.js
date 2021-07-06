f942d82fc613713a1d3ed04dfe470c91e47a9bc9
John Barker <jdabarker@gmail.com>
2021-07-06 11:54:28 +0800

    added get last workout route

--- controllers/api/workouts.js
+++ controllers/api/workouts.js
@@ -1,4 +1,17 @@
 const router = require("express").Router();
 const db = require("../../models");
 
+router.get("/", async (req, res) => {
+  try {
+    const lastWorkout = await db.Workout.findOne(
+      {},
+      {},
+      { sort: { createdAt: -1 } }
+    ).exec();
+    res.status(200).json(lastWorkout);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
 module.exports = router;
